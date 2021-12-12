b7e19b1b7509d3b501f8f88f237ce5a3cb88d2d4
Raito Bezarius <masterancpp@gmail.com>
2021-12-12 23:48:53 +0100

    auth: design profile mechanism à la AWS credentials

---
+++ examples/proxmox-profile.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+{
+  imports = [ ];
+  deployment.proxmox = {
+    profile = "default";
+    node = "askeladd"; # TODO: move me in profile.
+    pool = "demo-pool"; # TODO: move me in profile.
+    uefi = {
+      enable = true;
+      volume = "sata-vmdata";
+    };
+    network = [
+      ({ bridge = "vmbr2"; tag = 400; })
+      ({ bridge = "vmbr2"; tag = 300; })
+    ];
+    installISO = "local:iso/Raito-NixOS.iso";
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+}
+}
